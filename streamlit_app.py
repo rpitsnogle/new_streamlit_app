300e858b1abb79fb8edd7f19fcdf70ca394610b2
richard pitsnogle <120575978+rpitsnogle@users.noreply.github.com>
2023-07-23 02:38:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,8 +42,7 @@ try:
 except URLError as e:
   streamlit.error()
 # display the table
-
-streamlit.dataframe(fruityvice_normalized)
+# streamlit.dataframe(fruityvice_normalized)
 
 streamlit.header("The fruit load list contains:")
 #some funtion
